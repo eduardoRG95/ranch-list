525599b8f58a3a2577350e7d00e7fd7f8102fd94
luiz.garcez <luiz.garcez@NB-003754>
2021-04-02 21:14:31 -0300

    front

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,67 +1,94 @@
 import React, { useState } from "react";
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, Button, Alert } from "react-native";
+
+import { TextInput, List, Provider as PaperProvider } from 'react-native-paper';
 
 const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
+    {
+        id: "1",
+        title: "First Item",
+    },
+    {
+        id: "2",
+        title: "Second Item",
+    },
+    {
+        id: "3",
+        title: "Third Item",
+    },
 ];
 
 const Item = ({ item, onPress, style }: any) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
-    <Text style={styles.title}>{item.title}</Text>
-  </TouchableOpacity>
+    <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+        <Text style={styles.title}>{item.title}</Text>
+    </TouchableOpacity>
 );
 
 const App = () => {
-  const [selectedId, setSelectedId] = useState(null);
 
-  const renderItem = ({ item }: any) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    const [selectedId, setSelectedId] = useState(null);
+    const [text, setText] = React.useState('');
+
+    const [expanded, setExpanded] = React.useState(true);
+
+    const handlePress = () => setExpanded(!expanded);
 
     return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        style={{ backgroundColor }}
-      />
-    );
-  };
+        <SafeAreaView style={styles.container}>
+            <PaperProvider >
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
-      />
-    </SafeAreaView>
-  );
+                <TextInput
+                    style={styles.inputItens}
+                    label="Add Item"
+                    onChangeText={text => setText(text)}
+                />
+
+                <Button
+                    title="Salvar"
+                    onPress={() => Alert.alert('Simple Button pressed')} />
+
+                <List.Item
+                    title="First Item"
+                    description="Item description"
+                    left={props => <List.Icon {...props} icon="folder" />}
+                />
+                <List.Item
+                    title="First Item"
+                    description="Item description"
+                    left={props => <List.Icon {...props} icon="folder" />}
+                />
+                <List.Item
+                    title="First Item"
+                    description="Item description"
+                    left={props => <List.Icon {...props} icon="folder" />}
+                />
+            </PaperProvider>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
+    container: {
+        flex: 1,
+        paddingLeft: 5,
+        paddingRight: 5,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: "#e5e5e5",
+        borderBottomColor: "#333",
+        borderBottomWidth: 1,
+        padding: 10,
+        marginVertical: 8,
+        marginHorizontal: 10,
+    },
+    title: {
+        fontSize: 16,
+    },
+    inputItens: {
+        marginBottom: 12,
+    }
 });
 
 export default App;
