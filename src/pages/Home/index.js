1d34a420b0da94862b1ae5606873a85ec7f987ea
luiz.garcez <luiz.garcez@NB-003754>
2021-04-29 21:36:48 -0300

    ajustes efetuados

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,11 @@
-import React from 'react'
+import * as React from 'react';
+import { Modal, Portal, Text, Button, List, Provider } from 'react-native-paper';
 
-import { View, StyleSheet, StatusBar, Alert, Text  } from "react-native";
+import {  StyleSheet, StatusBar, Alert   } from "react-native";
 
 import { AsyncStorage } from 'react-native';
-
 import { Actions } from 'react-native-router-flux'
 
-
-import { Button , List, Provider as PaperProvider } from 'react-native-paper';
 
 // import Carousel from 'react-native-snap-carousel'
 // import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from '../../components/CarouselCardItem'
@@ -16,8 +14,14 @@ import { Button , List, Provider as PaperProvider } from 'react-native-paper';
 
 const Home = () => {
 
-const [expanded, setExpanded] = React.useState(true);
-const handlePress = () => setExpanded(!expanded);
+    const [visible, setVisible] = React.useState(false);
+
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+    const containerStyle = { backgroundColor: 'white', padding: 20 };
+
+    const [expanded, setExpanded] = React.useState(true);
+    const handlePress = () => setExpanded(!expanded);
 
     let data_object = {
         data: '2018-08-01',
@@ -25,12 +29,13 @@ const handlePress = () => setExpanded(!expanded);
         humidade: 40,
     };
     
-   var data = null;
-  const isCarousel = React.useRef(null)
+    var data = null;
+    
+    const isCarousel = React.useRef(null)
 
-  const goToList = () => {
-     Actions.list()
-  }  
+    const goToList = () => {
+        Actions.list()
+    }  
 
     const saveItem = async () => {
         try {
@@ -52,45 +57,34 @@ const handlePress = () => setExpanded(!expanded);
         }
    } 
 
-  return (
-      <View style={styles.container}>
-      <PaperProvider >
-              <List.Section style={styles.sectionList} title="Minhas Listas">
-                  <List.Item
-                      title="First Item"
-                      onPress={goToList}
-                      description="Item description"
-                      left={props => <List.Icon {...props} icon="folder" />}
-                  />
-                  <List.Item
-                      title="First Item"
-                      onPress={goToList}
-                      description="Item description"
-                      left={props => <List.Icon {...props} icon="folder" />}
-                  />
-          </List.Section>
-    
-
-
-          <Button  mode="contained" onPress={goToList}>
-              Crie Novas Listas
-            </Button>
-              <Button mode="contained" onPress={GetItem}>
-                  Crie Novas Listas
-            </Button>
-
-              {/* <Button mode="contained" onPress={saveItem}>
-                  salvar
-            </Button>
-
-              <Button mode="contained" onPress={GetItem}>
-                  buscar
-            </Button> */}
-      </PaperProvider>
-
-   </View>
-  )
+    return (
+                <Provider>
+                    <List.Section style={styles.sectionList} title="Minhas Listas">
+                    <List.Item
+                        title="First Item"
+                        onPress={goToList}
+                        description="Item description"
+                        left={props => <List.Icon {...props} icon="folder" />}
+                    />
+                    <List.Item
+                        title="First Item"
+                        onPress={goToList}
+                        description="Item description"
+                        left={props => <List.Icon {...props} icon="folder" />}
+                    />
+                    </List.Section>  
+                    <Portal>
+                        <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                            <Text>Example Modal.  Click outside this area to dismiss.</Text>
+                        </Modal>
+                    </Portal>
+                    <Button style={{ marginTop: 30 }} onPress={showModal}>
+                        Criar nova lista
+                        </Button>
+                </Provider>
+            )
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
